5d8cc8145dacd38d37c26fef0332bb0a4bdf3cb9
levanza1358 <kiki.manda16@gmail.com>
2025-10-09 21:30:20 +0700

    build: android apk + web->docs (2025-10-09 21:30, cff3e30)

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "812063108"
+    serviceWorkerVersion: "1075533487"
   }
 });
--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -28,7 +28,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "e355b471663a34b373b4b796d8212159
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "734b8e179b5f50b7bf9b2cb650f67dac",
+"flutter_bootstrap.js": "7eb505469240365e0bbeb12c93406b6d",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
